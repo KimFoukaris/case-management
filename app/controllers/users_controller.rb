2572f32709fc7239960520804b03c89975f1ec82
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-03 05:03:06 +0000

    fix errors in users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :current_user, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
@@ -9,11 +11,20 @@ class UsersController < ApplicationController
         session[:user_id] = @user.id
         redirect_to user_path(@user)
       else
-          render :new
+        render :new
       end
     end
 
+    def show
+      @message = params[:message] if params[:message]
+      @message ||= false
+    end
+
   private
+
+  def current_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
         params.require(:user).permit(
