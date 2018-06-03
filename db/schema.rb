e36501119c09b22dc722467f91492151cd0719d0
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-03 04:44:30 +0000

    fix admin boolean with default in new migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180603041626) do
+ActiveRecord::Schema.define(version: 20180603043601) do
 
   create_table "beneficiaries", force: :cascade do |t|
     t.string   "name"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 20180603041626) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_confirmation"
-    t.string   "admin"
+    t.boolean  "admin",                 default: false
   end
 
 end
