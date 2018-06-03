c6a5f3f3a9de46b4f7e01252035f40d25c34add5
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-03 04:35:14 +0000

    add create to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,26 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+          render :new
+      end
+    end
 
+  private
+
+  def user_params
+        params.require(:user).permit(
+          :name,
+          :title,
+          :email,
+          :password,
+          :password_confirmation,
+          :admin
+        )
+      end
 end
