54b2f28cbba279b23fade578d71e94ff1535e56f
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-04 16:18:18 +0000

    fix controller and form for nested routing for updates

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -3,4 +3,10 @@ class UpdatesController < ApplicationController
     @updates = Update.all
   end
 
+  def new
+    @beneficiary = Beneficiary.find(params[:beneficiary_id])
+    @update = @beneficiary.updates.build(beneficiary_id: params[:beneficiary_id], user_id: session[:user_id])
+  end
+
+
 end
