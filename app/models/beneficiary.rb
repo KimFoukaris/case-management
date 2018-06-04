cd56688c53671f624ec014a62dca56a18bfe289d
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-04 05:52:31 +0000

    add validations to Beneficiary

--- app/models/beneficiary.rb
+++ app/models/beneficiary.rb
@@ -2,4 +2,7 @@ class Beneficiary < ActiveRecord::Base
   has_many :updates
   has_many :users, through: :updates
 
+  validates :name, presence: true
+  validates :id_number, presence: true, uniqueness: true, length: { is: 7 }
+
 end
