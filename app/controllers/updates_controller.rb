d3dd4bdae3cd458db4aff30f203dcb3451ab24b4
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-04 16:45:48 +0000

    add create, show, edit to updates_controller

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,12 +1,35 @@
 class UpdatesController < ApplicationController
+  before_action :set_beneficiary, only: [:create, :show, :edit, :update, :destroy]
+
   def index
     @updates = Update.all
   end
 
   def new
-    @beneficiary = Beneficiary.find(params[:beneficiary_id])
-    @update = @beneficiary.updates.build(beneficiary_id: params[:beneficiary_id], user_id: session[:user_id])
+    @update = Update.new
   end
 
+  def create
+    @update = @beneficiary.updates.build(beneficiary_id: params[:beneficiary_id], notes: params[:update][:notes], user_id: session[:user_id])
+    if @update.save
+      redirect_to beneficiary_update_path(params[:beneficiary_id], @update), notice: 'Note was successfuly created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_beneficiary
+    @beneficiary = Beneficiary.find(params[:beneficiary_id])
+  end
+
+  def update_params
+    params.require(:update).permit(
+      :user_id,
+      :beneficiary_id,
+      :notes
+    )
+  end
 
 end
