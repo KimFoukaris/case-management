ae7b6e2716f4bcd5928dd40f8a739a2453257224
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-07 08:40:28 +0000

    fix error in update validations

--- app/models/update.rb
+++ app/models/update.rb
@@ -2,6 +2,8 @@ class Update < ActiveRecord::Base
   belongs_to :user
   belongs_to :beneficiary
 
-  validates :user_id, :beneficiary_id, :notes, presence: true
+  validates :notes, presence: true
+  validates :user_id, presence: true
+  validates :beneficiary_id, presence: true
 
 end
